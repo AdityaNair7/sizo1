472cc093283b825a4d39b7c5583b969cc7106f77
AdityaNair7 <161593981+AdityaNair7@users.noreply.github.com>
2026-07-31 12:18:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+URL = "https://www.lenovo.com/in/outletin/en/laptops/"
+
+CPU = [
+    "12650HX",
+    "13650HX",
+    "14650HX",
+]
+
+GPU = [
+    "RTX 4050",
+    "RTX 4060",
+    "RTX 4070",
+]
+
+RAM = 16
+SSD = 1024
+
+MAX_PRICE = 70000
