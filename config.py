f4596c6d2f0d8c2854450ac46a031fbd2902869e
AdityaNair7 <161593981+AdityaNair7@users.noreply.github.com>
2026-07-31 16:26:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,27 @@
-URL = "https://www.lenovo.com/in/outletin/en/laptops/"
+URL = "https://openapi.lenovo.com/in/outletin/en/ofp/search/dlp/product/query/get/_tsc"
+
+PAGE_FILTER_ID = "afdcd3f7-d8e6-4e9e-a76a-d6060dc75ae9"
+
+MAX_PRICE = 70000
 
 CPU = [
     "12650HX",
     "13650HX",
     "14650HX",
+    "12700H",
+    "13700H",
+    "14700HX"
 ]
 
 GPU = [
+    "RTX™ 4050",
     "RTX 4050",
+    "RTX™ 4060",
     "RTX 4060",
-    "RTX 4070",
+    "RTX™ 4070",
+    "RTX 4070"
 ]
 
 RAM = 16
-SSD = 1024
 
-MAX_PRICE = 70000
+SSD = 1024
