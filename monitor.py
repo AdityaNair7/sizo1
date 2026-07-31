1c85c7fbff2e8467543a6cb6bb410072c90fdd90
AdityaNair7 <161593981+AdityaNair7@users.noreply.github.com>
2026-07-31 12:48:58 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -3,19 +3,30 @@ from playwright.sync_api import sync_playwright
 URL = "https://www.lenovo.com/in/outletin/en/laptops/"
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
+    browser = p.chromium.launch(
+        headless=True,
+        args=["--disable-dev-shm-usage"]
+    )
 
     page = browser.new_page()
 
-    page.goto(URL, wait_until="networkidle", timeout=120000)
+    print("Opening Lenovo...")
 
-    page.wait_for_timeout(10000)
+    page.goto(
+        URL,
+        wait_until="domcontentloaded",
+        timeout=120000
+    )
+
+    print("Waiting for JavaScript...")
+
+    page.wait_for_timeout(20000)
 
     html = page.content()
 
     with open("output.html", "w", encoding="utf-8") as f:
         f.write(html)
 
-    print("Page downloaded successfully.")
+    print("Saved output.html")
 
     browser.close()
